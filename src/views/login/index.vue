468024b7ede0584480de2375fe1afd0d716e2344
conghui <740364177@qq.com>
2020-07-30 16:55:04 +0800

    表单自动验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,19 +6,19 @@
         <img src="../../assets/img/logo_index.png" alt />
       </div>
       <!-- 表单 -->
-      <el-form style="margin-top:20px">
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form style="margin-top:20px" :model="loginForm" :rules="rules" ref="loginForm">
+        <el-form-item prop='mobile'>
+          <el-input placeholder="请输入手机号"  v-model="loginForm.mobile"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:60%"  placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input style="width:60%"  placeholder="请输入验证码"  v-model="loginForm.code"></el-input>
           <el-button plain style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已同意</el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="loginForm.checked">我已同意</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%" @click="onSubmit">登录</el-button>
+          <el-button type="primary" style="width:100%" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -26,7 +26,43 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        checked: false
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '手机号不能为空' },
+          { pattern: /^1[3-9]\d{9}/, message: '您的手机号不正确' }
+        ],
+        code: [
+          {
+            required: true, message: '验证码不为空'
+          },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }
+        ],
+        checked: [
+          {
+            validator: function (rule, value, callback) {
+              value ? callback() : callback(new Error('您必须同意条款'))
+            }
+          }
+        ]
+      },
+      methods: {
+        login () {
+          this.$refs.loginForm.validator().then(() => {
+
+          })
+        }
+      }
+    }
+  }
+}
 </script>
 
 <style lang = 'less' scoped>
