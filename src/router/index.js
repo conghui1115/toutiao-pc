91886e2c1d8bad233e7577a713a519554332e454
conghui <740364177@qq.com>
2020-07-30 14:58:11 +0800

    准备资源

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 // 导入vue-router
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 // 配置路由表
@@ -10,15 +10,15 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    shoye
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
