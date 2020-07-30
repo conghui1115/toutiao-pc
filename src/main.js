d174afdab02776aecd6bf4cc448dd6b73fdab47d
conghui <740364177@qq.com>
2020-07-30 14:35:11 +0800

    ll

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  // 挂载
+  router,
+  render: h => h(App)
+}).$mount('#app')
